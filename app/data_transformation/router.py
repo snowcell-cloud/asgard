a520c06a450a416a2a235faac3c154eecabe7a1d
amrit <amrit@faralpha.com>
2025-08-27 10:45:47 +0000

    fix: Update router tag from 'transform' to 'Data Transformation'

--- app/data_transformation/router.py
+++ app/data_transformation/router.py
@@ -9,7 +9,7 @@ from app.data_transformation.client import get_spark_client, SparkApplicationCli
 from app.data_transformation.service import TransformationService
 from app.airbyte.client import get_airbyte_client_dependency, AirbyteClient
 
-router = APIRouter(tags=["transform"])
+router = APIRouter(tags=["Data Transformation"])
 
 
 def get_transformation_service(
